97eaa1830c84610a72a66a1de499dcb2ef53d3ce
jadefr <jade@ice.ufjf.br>
2020-03-11 21:10:07 -0300

    Add destaques section

---
+++ module/Application/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card-component-container">
+        <section>
+            <h1>CARD</h1>
+        </section>
+    </div>
+</template>
+
+<script type="module">
+    export default {
+        props: {
+            title: String
+        }
+    }
+</script>
+
+<style  lang="scss" scoped>
+    .card-component-container {
+        background: red;
+        color: greenyellow;
+        font-size: 2em;
+
+        section {
+            padding: 200px;
+            background: yellow;
+        }
+    }
+</style>
